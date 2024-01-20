7e0e7b1de664968ebb2619c791b5f046f91bd868
IslamQodeara7 <islamqodeara21@gmail.com>
2024-01-20 10:11:21 +0200

    Sum1ToN

---
+++ src/main/java/com/mycompany/sum1ton/Sum1ToN.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sum1ton;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Islam Mohamed
+ */
+public class Sum1ToN {
+
+    public static void main(String[] args) {
+        long n;
+        Scanner scan = new Scanner(System.in);
+        n= scan.nextLong();
+        System.out.println(n*(n+1)/2);
+        
+        
+    }
+}
